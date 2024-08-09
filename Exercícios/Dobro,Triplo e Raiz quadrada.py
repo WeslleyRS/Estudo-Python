37877e11a557e216f532fb8a371111edd4d71e6e
Weslley Rodrigues <89474953+WeslleyRS@users.noreply.github.com>
2024-08-09 03:20:02 -0300

    Update Dobro,Triplo e Raiz quadrada.py

---
+++ Exercícios/Dobro,Triplo e Raiz quadrada.py
@@ -0,0 +1,13 @@
+usuario = input ('Digite o seu úsuario: ')
+
+print (f'Seja Bem-vindo ao sistema Dobro, Triplo e Raiz Quadrada {usuario}!')
+
+valor = float ((input ('Digite um valor: ')))
+
+dobro = valor * 2
+
+triplo = valor * 3
+
+raiz = valor ** (1/2)
+
+print (f'O número escolhido foi {valor} o dobro dele é {dobro} o seu triplo {triplo} e sua Raiz Quadrada {raiz :.2}')
