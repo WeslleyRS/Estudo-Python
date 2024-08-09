2017e83459a93bd11c87eb8e46fbccec9fedcb93
Weslley Rodrigues <89474953+WeslleyRS@users.noreply.github.com>
2024-08-09 03:20:19 -0300

    Update Sucessor e Antecessor.py

---
+++ Exercícios/Sucessor e Antecessor.py
@@ -0,0 +1,7 @@
+usuario = input ('Digite o úsuario: ')
+
+print (f'Seja Bem-Vindo(a) ao sistema de números antecessores e sucessores {usuario}!')
+
+numero = int (input('Digite um número: '))
+
+resultado = input (f'O número que voce escolheu foi {numero} o seu antecessor é {numero-1} e o seu sucessor é {numero+1}')
